17dbb6e51fbc923dc515b9c7cb75860091a72957
martijnbentum <bentummartijn@gmail.com>
2024-04-04 18:25:18 +0200

    bas webserver api urls

---
+++ url.py
@@ -0,0 +1,3 @@
+pipeline='https://clarin.phonetik.uni-muenchen.de/BASWebServices/services/runPipeline'
+load_indicator='https://clarin.phonetik.uni-muenchen.de/BASWebServices/services/getLoadIndicator'
+
