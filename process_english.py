5804ebcb81d409b2f1558311c89830b857ca4b67
martijnbentum <bentummartijn@gmail.com>
2024-05-14 15:09:17 +0200

    updated to deal with accents

--- process_english.py
+++ process_english.py
@@ -12,6 +12,10 @@ textgrid_directory = directory + 'textgrids/'
 audio_directory = directory + 'clips/'
 transcription_directory = directory + 'transcriptions/'
 
+
+
+
+
 def load_validated():
     with open(directory + 'validated.tsv', 'r') as f:
         t = f.read().split('\n')
@@ -40,6 +44,36 @@ def make_files(force_make = False):
 
 def make_force_align_pipeline(files = None):
     if not files: files = make_files()
-    p = mauser.Pipeline(files, textgrid_directory, language = 'eng-EN')
+    p = mauser.Pipeline(files, textgrid_directory, language = None, 
+        language_dict = make_accent_dict())
     return p
     
+
+def make_accent_dict():
+    t = load_validated()
+    d = {}
+    for line in t[1:]:
+        if line == '': continue
+        line = line.split('\t')
+        sentence_id = line[1]
+        accent = line[-1]
+        if accent == '': accent = 'eng-US'
+        if accent == 'newzealand': accent = 'eng-NZ'
+        if accent == 'wales': accent = 'eng-GB'
+        if accent == 'malaysia': accent = 'eng-US'
+        if accent == 'indian': accent = 'eng-GB'
+        if accent == 'australia': accent = 'eng-AU'
+        if accent == 'bermuda': accent = 'eng-US'
+        if accent == 'philippines': accent = 'eng-US'
+        if accent == 'us': accent = 'eng-US'
+        if accent == 'southatlandtic': accent = 'eng-US'
+        if accent == 'hongkong': accent = 'eng-GB'
+        if accent == 'african': accent = 'eng-GB'
+        if accent == 'singapore': accent = 'eng-US'
+        if accent == 'canada': accent = 'eng-US'
+        if accent == 'other': accent = 'eng-US'
+        if accent == 'ireland': accent = 'eng-GB'
+        if accent == 'scotland': accent = 'eng-SC'
+        if accent == 'england': accent = 'eng-GB'
+        d[sentence_id] =accent
+    return d
