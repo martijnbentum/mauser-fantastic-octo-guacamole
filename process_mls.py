de6956227202729021389fbe1f94d7bc1c870745
martijnbentum <bentummartijn@gmail.com>
2025-01-02 14:13:44 +0100

    added css10 hungarian

--- process_mls.py
+++ process_mls.py
@@ -15,8 +15,6 @@ def load_mls(language):
         dataset = load_dataset("facebook/multilingual_librispeech", language)
     return dataset
 
-
-
 def make_txt_file(item, split, language):
     txt_dir = f'{mls_dir}{language}/txt/'
     directory = f'{txt_dir}{split}/'
@@ -58,28 +56,59 @@ def handle_item(item, split, language):
     make_audio(item, audio_filename)
     return txt_filename, audio_filename
 
-def handle_items(items, split, language):
+def handle_items(items, split, language, start = None,
+    end = None):
     output = []
     filename =f'{mls_dir}{language}/{language}_{split}.json'
+    index = 0
     for item in progressbar(items):
+        if start is None: pass
+        elif index < start:
+            index += 1
+            continue 
         txt_filename, audio_filename= handle_item(item,
             split, language)
         json_line = {"audio_filename": audio_filename, 
             "text_filename": txt_filename}
         output.append(json_line)
+        if end is None: pass
+        elif index >= end:
+            break
     save_output(output, filename)
     return output
 
 def handle_language(language, items = None,
-    splits = ['dev', 'test', 'train']):
+    splits = ['dev', 'test', 'train'], 
+    start = None, end = None):
     if not items:
         items = load_mls(language)
     for key in splits:
         print(f'handling {key}')
         split = items[key]
-        handle_items(split, key, language)
+        handle_items(split, key, language, start,end)
 
 
+def handle_css10_hungarian():
+    directory = '/vol/mlusers/mbentum/mls/hungarian/'
+    with open(f'{directory}transcript.txt') as f:
+        t = f.read().split('\n')
+    output = []
+    for line in t:
+        f, transcript,_,_ = line.split('|')
+        f = f.split('/')[-1]
+        text_filename = Path(f'{directory}txt/test/' + f.replace('wav','txt'))
+        audio_filename = Path(f'{directory}audio/test/'+ f)
+        o = {'audio_filename':str(audio_filename),
+            'text_filename':str(text_filename)}
+        output.append(o)
+        with open(str(text_filename), 'w') as f:
+            f.write(transcript)
+        if not text_filename.exists():
+            raise Exception(f'{text_filename} does not exist')
+        if not audio_filename.exists():
+            raise Exception(f'{audio_filename} does not exist')
+        # print(f,text_filename,'\n',o,transcript)
+    save_output(output, f'{directory}hungarian_test.json')
 
 
 def save_output(output, output_file):
@@ -99,6 +128,7 @@ def handle_force_align(language, splits = ['dev', 'test', 'train']):
     if language == 'polish': language_code = 'pol-PL'
     if language == 'english': language_code = 'eng-US'
     if language == 'german': language_code = 'deu-DE'
+    if language == 'hungarian': language_code = 'hun-HU'
     for key in splits:
         print(f'handling {key} {language}, {language_code}')
         files=load_files(f'{mls_dir}{language}/{language}_{key}.json')
@@ -118,6 +148,9 @@ def force_align_english(splits = ['dev', 'test']):
 
 def force_align_german(splits = ['dev', 'test']):
     handle_force_align('german', splits)
+
+def force_align_hungarian(splits = ['test']):
+    handle_force_align('hungarian', splits)
 
     
     
